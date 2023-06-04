c4da0025a8a1ad27b31eac9061359a2b1a55a84f
dakyumlee <oicrcutie@gmail.com>
2023-06-04 19:15:52 +0900

    js

---
+++ index.js
@@ -0,0 +1,56 @@
+const arr = [1,2,3,4];
+arr.forEach(console.log);
+
+let person = {
+  name: 'jeonghan',
+  gender: 'male',
+  say: function() {
+    console.log('안녕 나는 '+this.name+' 입니다.')
+  }
+};
+
+console.log(typeof person);
+console.log(person);
+
+person.say();
+
+switch (month) {
+  case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+    days = 31;
+    break;
+  case 4: case 6: case 9: case 11:
+    days = 30;
+    break;
+  case 2:
+    // 윤년 계산 알고리즘
+    // 1. 년도가 4로 나누어 떨어지는 해는 윤년(2000, 2004, 2008, 2012, 2016, 2020…)
+    // 2. 그 중에서 년도가 100으로 나누어 떨어지는 해는 평년(2000, 2100, 2200...)
+    // 3. 그 중에서 년도가 400으로 나누어 떨어지는 해는 윤년(2000, 2400, 2800...)
+    days = ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) ? 29 : 28;
+    break;
+  default:
+    console.log('Invalid month');
+}
+
+console.log(days); // 29
+
+for (var i = 1; i <= 6; i++) {
+  for (var j = 1; j <= 6; j++) {
+    if (i + j === 6) console.log(`[${i}, ${j}]`);
+  }
+}
+
+let count = 0;
+
+do {
+	console.log(count);
+	count++;
+} while (count < 3);
+
+foo: {
+  console.log(1);
+  break foo; // foo 레이블 블록문을 탈출한다.
+  console.log(2);
+}
+
+console.log('Done!');
